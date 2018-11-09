43e7c7e58f06ff90fd92741b795cd73eeef3d34f
VadimPoltoratskiy <vadjan87heavymetall@gmail.com>
2018-11-09 20:11:44 +0200

    09.11.2018 Done Fixes in controller and endpoint

--- src/main/java/com/thymeleaf/course/controller/CompanyController.java
+++ src/main/java/com/thymeleaf/course/controller/CompanyController.java
@@ -4,6 +4,7 @@ import com.thymeleaf.course.domain.model.entity.Company;
 import com.thymeleaf.course.domain.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Controller
-//@RequestMapping(value = "/api/companies")
+@RequestMapping(value = "/api/companies")
 public class CompanyController {
 
     @Autowired
@@ -20,10 +21,10 @@ public class CompanyController {
 
     /* TODO You need to implement endpoint here. */
     /* TODO Endpoint should be map by Get method. You need to retrieve data from service, and return it to the 'company.html'. */
-    @RequestMapping(value="/companies", method = RequestMethod.GET)
-    public String getCompanies(ModelMap model) {
+    @GetMapping
+    public String getCompanies(Model model) {
         List<Company> companyList = companyService.getAllCompanies();
-        model.put("companyList", companyList);
+        model.addAttribute("companyList", companyList);
         return "showCompanies";
     }
 
